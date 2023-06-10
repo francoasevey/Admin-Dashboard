7827e86bb8b15c07c1c6b543f58f31244a1ea63f
francoromeo <francoromeo5012@gmail.com>
2023-06-09 22:23:43 -0300

    dark theme

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector("#menu-btn");
 const slideMenu = document.querySelector("aside");
 const closeBtn = document.querySelector("#closeBtn");
+const themeSwitcher = document.querySelector(".switch-theme");
 
 menuBtn.addEventListener("click", ()=>{
     slideMenu.style.display = "block";
@@ -8,4 +9,11 @@ menuBtn.addEventListener("click", ()=>{
 
 closeBtn.addEventListener("click", ()=>{
     slideMenu.style.display = "none";
+})
+
+themeSwitcher.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark-theme-details");
+
+    themeSwitcher.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeSwitcher.querySelector("span:nth-child(2)").classList.toggle("active");
 })
